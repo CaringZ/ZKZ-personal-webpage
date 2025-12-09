a47655783580a4ed0264680295407ed7577791c4
CaringZ <1035067533@QQ.com>
2025-12-09 20:47:09 +0800

    Update build config for root domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,9 @@
-const repo = 'ZKZ-personal-webpage';
-const isProd = process.env.NODE_ENV === 'production';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   reactStrictMode: false,
-  // output: 'export', // Temporarily disabled for development
-  assetPrefix: isProd ? `/${repo}` : '',
-  basePath: isProd ? `/${repo}` : '',
-
-  // 恢复这一块！让客户端JS可以访问到这个环境变量
   env: {
-    NEXT_PUBLIC_BASE_PATH: isProd ? `/${repo}` : '',
+    NEXT_PUBLIC_BASE_PATH: '', 
   },
 
   trailingSlash: true,
